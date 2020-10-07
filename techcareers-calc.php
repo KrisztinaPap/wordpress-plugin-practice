9aa14fe3c917e63275406adf1218beef8a75d66e
Krisztina Pap <krisztina.pap@outlook.com>
2020-10-07 14:36:39 -0600

    Adds main php file; Adds plugin description;

---
+++ techcareers-calc.php
@@ -0,0 +1,13 @@
+
+<?php
+/**
+ * Plugin Name:       TECHCareers Calculator Plugin
+ * Plugin URI:        https://TECHCareers.ca
+ * Description:       A simple PHP calculator
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            TECHCareers instructor (classroom lesson project)
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
